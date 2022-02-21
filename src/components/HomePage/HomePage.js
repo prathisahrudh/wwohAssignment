c5d7ce44ff19c1b219d62abdbfcfd42d3c4fb2d6
prathisahrudh <sahrudhprathi@hotmail.co.uk>
2022-02-21 22:00:42 +0530

    Lower Loading Animation Time and write functions to fetch data through API

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,20 +1,39 @@
 import "./HomePage.css";
 
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const HomePage = () => {
   // API's
   const postsApi = "https://jsonplaceholder.typicode.com/posts";
   const photosApi = "https://jsonplaceholder.typicode.com/photos";
 
-  const somePosts = []
-  const somePhotos = []
-  return (
-      const getSomePhotos = useEffect(() => {
-          const result = fetch(photosApi).then(res => res.json())
-      })
-  <div>HomePage</div>
-  );
+  //State for Storing Full API Data
+  const [postsData, setPostsData] = useState([]);
+  const [photosData, setPhotosData] = useState([]);
+
+  const getData = async () => {
+    //   Function for API Calls
+    const fetchData = async (x) => {
+      let res = await fetch(x);
+      let data = await res.json();
+      return data;
+    };
+    const getPosts = await fetchData(postsApi);
+    const getPhotos = await fetchData(photosApi);
+    setPostsData(getPosts);
+    setPhotosData(getPhotos);
+  };
+
+  // UseEffect for Stroing Over API Data
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const postsFinal = [postsData[0], postsData[1]];
+  const photosFinal = [photosData[0], photosData[1]];
+//   console.log(postsFinal[0]);
+
+  return <div>This is Home Page</div>;
 };
 
 export default HomePage;
--- src/components/Main.js
+++ src/components/Main.js
@@ -10,6 +10,7 @@ const Main = () => {
     <>
       <Navbar />
       <HomePage />
+      HI
     </>
   )
 }
